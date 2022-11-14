04d8c223f2584e8e9efea7c0173b4ce4fce739d7
yeonjeon9 <yeonjeon9@gachon.ac.kr>
2022-11-14 19:14:16 +0900

    teamproject_1

--- TermProject_Team7/data_structure.h
+++ TermProject_Team7/data_structure.h
@@ -1,4 +1,46 @@
 #pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <string.h>
 /*
 1. linked list, sturct array »ý¼º
 */
+
+typedef struct data {
+
+	int tag;
+	char data_registered[20];
+	char fee_paid[10];
+	char name[50];
+	int age;
+	char organization[50];
+	char job[50];
+
+} people;
+
+typedef struct data {
+
+	int tag;
+	char data_registered[20];
+	char fee_paid[10];
+	char name[50];
+	int age;
+	char organization[50];
+	char job[50];
+	struct data* next;
+
+} LL_people;
+
+LL_people* createNode(int tag, char data_registered[], char fee_paid[], char name[], int age, char organization[], char job[]) {
+	data* node = (data*)malloc(sizeof(data));
+	node->tag=tag;
+	strcpy(node->data_registered, data_registered);
+	strcpy(node->fee_paid, fee_paid);
+	strcpy(node->name, name);
+	node->age = age;
+	strcpy(node->organization, organization);
+	strcpy(node->job, job);
+
+	node->next = NULL;
+	return node;
+}
