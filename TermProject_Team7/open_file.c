d163c29d64748a5be2ad9e97954456b1beaf8217
yeonjeon9 <113374566+yeonjeon9@users.noreply.github.com>
2022-11-24 14:55:42 +0900

    Update open_file.c
    셋업 구조체 안에 구조체 넣고 노드 만들었습니다

--- TermProject_Team7/open_file.c
+++ TermProject_Team7/open_file.c
@@ -1,46 +1,41 @@
 #pragma once
 #define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-struct DATA {
+typedef struct data {
 
 	int tag;	//registration number-unique integer
-	char data_registered[20];	//yyyy-mm-dd
-	char fee_paid[10];	//"yes" or "no"
+	char dataRegistered[20];	//yyyy-mm-dd
+	char feePaid[10];	//"yes" or "no"
 	char name[25];	//char[25]
 	int age;	//integer
 	char organization[30];	//company or university; char[30]
 	char job[15];	//student, professor, staff, executive, engineer, marketer; char[15]
 
-};
+} PEOPLE;
 
-struct NODE {
+typedef struct node {
 
-	int tag;	//registration number-unique integer
-	char data_registered[20];	//yyyy-mm-dd
-	char fee_paid[10];	//"yes" or "no"
-	char name[25];	//char[25]
-	int age;	//integer
-	char organization[30];	//company or university; char[30]
-	char job[15];	//student, professor, staff, executive, engineer, marketer; char[15]
-	struct NODE* next;
+	PEOPLE people;
+	struct node* next;
 
-};
+}NODE;
 
-struct NODE* createNewNode(struct NODE* head, struct DATA* data, int i) {
-	struct NODE* newNode = (struct NODE*)malloc(sizeof(struct NODE));
+struct node* createNewNode(struct node* head, struct data* data, int i) {
+	struct node* newNode = (struct node*)malloc(sizeof(struct node));
 	if (newNode == NULL) {
-		printf("newNode == NULL\n");
+		printf("newNode == NULL \n");
 		exit(1);
 	}
-	newNode->tag = data[i].tag;
-	strcpy(newNode->data_registered, data[i].data_registered);
-	strcpy(newNode->fee_paid, data[i].fee_paid);
-	strcpy(newNode->name, data[i].name);
-	newNode->age = data[i].age;
-	strcpy(newNode->organization, data[i].organization);
-	strcpy(newNode->job, data[i].job);
+	newNode->people.tag = data[i].tag;
+	strcpy(newNode->people.dataRegistered, data[i].dataRegistered);
+	strcpy(newNode->people.feePaid, data[i].feePaid);
+	strcpy(newNode->people.name, data[i].name);
+	newNode->people.age = data[i].age;
+	strcpy(newNode->people.organization, data[i].organization);
+	strcpy(newNode->people.job, data[i].job);
 
 	newNode->next = NULL;
 
