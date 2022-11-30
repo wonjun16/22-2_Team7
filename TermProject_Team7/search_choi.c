50cddbee3ea79db2d2a8e19b7ab352c37476c81c
SuJin Park <112803684+soojin-byte@users.noreply.github.com>
2022-11-30 11:54:20 +0900

    Update search_choi.c

--- TermProject_Team7/search_choi.c
+++ TermProject_Team7/search_choi.c
@@ -1,4 +1,37 @@
 /*
-2. °¢ ÀÚ·á±¸Á¶¿¡ ´ëÇØ choi ¼­Ä¡
-linked list, sturct array¿¡ ´ëÇØ °¢°¢ ±¸Çö
+2. ê° ìë£Œêµ¬ì¡°ì— ëŒ€í•´ choi ì„œì¹˜
+linked list, sturct arrayì— ëŒ€í•´ ê°ê° êµ¬í˜„
 */
+
+//----- search choi
+void searchChoiArray(PEOPLE* people) {
+
+    printf("Search Choi using Array \n");
+    int i = 0;
+    while (i < 30) {
+        if (strstr((people + i)->name, "Choi") != NULL) { //if name include 'choi', data output
+            printPeople(*(people + i));
+        }
+        i++;
+    }
+    printf("\n");
+}
+
+void searchChoiLinkedList(NODE* head) {
+   NODE* tmp = head->next; //cursor
+    printf("Search Choi using LinkedList \n");
+    while (tmp) {
+        if (strstr(tmp->people.name, "Choi") != NULL) { //if name include 'choi', data output
+            printPeople(tmp->people);
+        }
+        tmp = tmp->next; //next node
+    }
+    printf("\n");
+}
+
+
+//main
+
+int main(){
+  searchChoiArray(peoples);
+   searchChoiLinkedList(head);
