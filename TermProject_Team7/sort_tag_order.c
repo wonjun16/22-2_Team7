a5151a8570d77b77ce5a0acf518a2fb6df66949f
bbbbnag <92293058+bbbbnag@users.noreply.github.com>
2022-12-08 18:19:32 +0900

    Update sort_tag_order.c
    기존 꺼가 tag만 바뀌어서 태그랑 array 전부 바뀌게 다시 수정했습니다

--- TermProject_Team7/sort_tag_order.c
+++ TermProject_Team7/sort_tag_order.c
@@ -1,24 +1,20 @@
 /*
 4. array의 자료들을 tag순서로 정렬
 */
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include "data_structure.h"
+#include "sort_tag_order.h"
 
-void sort_tag_order(people PEOPLE[]) {
+void sort_tag_order(PEOPLE peoples[]) {
 
-	int hold, current, start;
-	
-	for (start = 0; start < 30; start++) {
-		int smallest = start;
+    PEOPLE temp;
 
-		for (current = start + 1; current < 30; current++) {
-			if (PEOPLE[current].tag < PEOPLE[smallest].tag)
-				smallest = current;
-		}
-
-		hold = PEOPLE[start].tag;
-		PEOPLE[start].tag = PEOPLE[smallest].tag;
-		PEOPLE[smallest].tag = hold;
-	}
+    for (int i = 0; i < 29; i++) {
+        for (int j = 0; j < 29; j++) {
+            if (peoples[j].tag > peoples[j + 1].tag) { // Use bubble sort 
+                // swap
+                temp = peoples[j];                     
+                peoples[j] = peoples[j + 1];
+                peoples[j + 1] = temp;
+            }
+        }
+    }
 }
